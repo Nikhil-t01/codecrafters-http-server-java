82d74a76841b48015c4da580122c1cbf5c90e654
Nikhil-t01 <nikhiltummidi2002@gmail.com>
2024-03-12 02:26:30 +0530

    chore: send path based response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -11,10 +13,25 @@ public class Main {
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
       Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+
+      try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+        String startLine = inputReader.readLine();
+        String[] parts = startLine.split("\\s+");
+
+        if (parts.length >= 2) {
+         Boolean isPathMatched = "/".equals(parts[1]);
+          clientSocket.getOutputStream().write(getResponse(isPathMatched).getBytes(StandardCharsets.UTF_8));
+        }
+      } catch (Exception e) {
+        System.out.print("Exception in reading input: " + e.getMessage());
+      }
       System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  private static String getResponse(Boolean isPathMatched) {
+    return isPathMatched ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
   }
 }
