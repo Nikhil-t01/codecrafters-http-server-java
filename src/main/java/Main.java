70ff2d6ea2b50639d2d2c959daa5456ff1b1ca63
Nikhil-t01 <nikhiltummidi2002@gmail.com>
2024-03-12 02:11:13 +0530

    chore: send 200 response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,17 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    
-    try {
-      serverSocket = new ServerSocket(4221);
+    try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
       System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
